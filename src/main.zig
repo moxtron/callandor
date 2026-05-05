9f6bc6f7b6fb409e63a8b4c83b83848728a8f1c1
moxtron <moxtron@outlook.com>
2026-05-05 11:23:28 +0200

    #1 toolchain verified with blinking led

--- src/main.zig
+++ src/main.zig
@@ -2,8 +2,21 @@ const std = @import("std");
 const microzig = @import("microzig");
 
 const rpi = microzig.hal;
+const time = rpi.time;
 
+// Compile-time pin configuration
+const pin_config = rpi.pins.GlobalConfiguration{
+    .GPIO25 = .{
+        .name = "led",
+        .direction = .out,
+    },
+};
 
 pub fn main() void {
+    const pins = pin_config.apply();
 
+    while (true) {
+        pins.led.toggle();
+        time.sleep_ms(250);
+    }
 }
