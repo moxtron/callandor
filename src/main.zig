5abd9f91c8ad7d2defe79db8f46b90c122c66a4a
moxtron <moxtron@outlook.com>
2026-05-07 19:18:22 +0200

    setting up PWM pin configurations in `main.zig`, including ones needed later. use this pinout as a reference.

--- src/main.zig
+++ src/main.zig
@@ -6,17 +6,37 @@ const time = rpi.time;
 
 // Compile-time pin configuration
 const pin_config = rpi.pins.GlobalConfiguration{
-    .GPIO25 = .{
-        .name = "led",
+    .GPIO22 = .{
+        .name = "esc",
         .direction = .out,
+        .function = .PWM3_A,
+    },
+    .GPIO16 = .{
+        .name = "aileron_left",
+        .direction = .out,
+        .function = .PWM0_A,
+    },
+    .GPIO17 = .{
+        .name = "aileron_right",
+        .direction = .out,
+        .function = .PWM0_B,
+    },
+    .GPIO18 = .{
+        .name = "elevator",
+        .direction = .out,
+        .function = .PWM1_A,
+    },
+    .GPIO19 = .{
+        .name = "rudder",
+        .direction = .out,
+        .function = .PWM1_B,
     },
 };
 
 pub fn main() void {
     const pins = pin_config.apply();
+    _ = pins;
 
     while (true) {
-        pins.led.toggle();
-        time.sleep_ms(250);
     }
 }
