f6749b7bcb36c946d8117b8e489d90bfbaa43b08
moxtron <moxtron@outlook.com>
2026-05-07 17:57:13 +0200

    cleaned up imports

--- src/pwm.zig
+++ src/pwm.zig
@@ -1,6 +1,3 @@
-const std = @import("std");
-const microzig = @import("microzig");
-
 pub const div:  u8  = 125;       // 125MHz / 125 -> 1us per tick
 pub const wrap: u16 = 19_999;    // 20ms period  -> 50Hz (0 .. 19_999 = 20_000)
 pub const frac: u8  = 0;         // fraction is zero, because it's not needed for this project
