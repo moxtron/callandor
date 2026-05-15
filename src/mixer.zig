002e39b0de5f2446e4da51a132cb5a11486610ad
moxtron <moxtron@outlook.com>
2026-05-15 21:50:58 +0200

    added mixer logic and PilotControls struct. optimized with comptime.

---
+++ src/mixer.zig
@@ -0,0 +1,67 @@
+const servo = @import("servo.zig");
+const esc = @import("esc.zig");
+
+/// Used to make the RC channels more convenient.
+const PilotControls = struct {
+    ailerons: u16,
+    elevator: u16,
+    throttle: u16,
+    rudder:   u16,
+    aux1:     u16,
+    aux2:     u16,
+    aux3:     u16,
+    aux4:     u16,
+    aux5:     u16,
+    aux6:     u16,
+    aux7:     u16,
+    aux8:     u16,
+    aux9:     u16,
+    aux10:    u16,
+    aux11:    u16,
+    aux12:    u16,
+};
+
+pub fn genPilotControlsFromChannels(channels: [16]u16) PilotControls {
+    return .{
+        .ailerons = channels[0],
+        .elevator = channels[1],
+        .throttle = channels[2],
+        .rudder   = channels[3],
+        .aux1     = channels[4],
+        .aux2     = channels[5],
+        .aux3     = channels[6],
+        .aux4     = channels[7],
+        .aux5     = channels[8],
+        .aux6     = channels[9],
+        .aux7     = channels[10],
+        .aux8     = channels[11],
+        .aux9     = channels[12],
+        .aux10    = channels[13],
+        .aux11    = channels[14],
+        .aux12    = channels[15],
+    };
+}
+
+pub fn mix(pc: PilotControls, motor: esc.Esc , comptime servos: []servo.Servo, failsafe: bool) void {
+    if(!failsafe) {
+        motor.setThrottle(scaleToUs(pc.throttle, motor.config.min_us, motor.config.max_us));
+        inline for (servos) |s| {
+            switch (s.config.servo_type) {
+                .aileron  => s.setPulse(scaleToUs(pc.ailerons, s.config.min_us, s.config.max_us)),
+                .elevator => s.setPulse(scaleToUs(pc.elevator, s.config.min_us, s.config.max_us)),
+                .rudder   => s.setPulse(scaleToUs(pc.rudder,   s.config.min_us, s.config.max_us)),
+                else => @compileError("Unknown servo type reached the mixer! (see `fn mix()`)"),
+            }
+        }
+    } else {
+
+    }
+}
+// NOTE: the integer divisions are fine for now, but maybe later we should leverage the hardware SIO divider. it does integer division in 8 cycles instead of the 20-40 cycles.
+
+fn scaleToUs(crsf: u16, comptime min_us: u16, comptime max_us: u16) u16 {
+    const crsf_min =  172; // minimum for CRSF values
+    const crsf_max = 1811; // maximum for CRSF values
+    // using saturating subtraction to guard against a glitchy signal (crsf < 172)
+    return (crsf -| crsf_min) * (max_us - min_us) / (crsf_max - crsf_min) + min_us;
+}
